0c5207b6933246d938089985838589a7a6c674a5
Veloooo <nazguleczek@gmail.com>
2017-06-03 16:08:15 +0200

    Add files via upload

--- ogrze.php
+++ ogrze.php
@@ -1,3 +1,22 @@
 <?php include('menu.php'); ?>
 <h1> O grze</h1>
+Warcraft III: WBE to jedna z najbardziej oczekiwanych premier lata 2017 roku. Gra powstała z ogromnym zaangażowaniem jego twórców, którzy włożyli w ten projekt całe swoje serce.WBE przenosi graczy do uniwersum znanego z wielu gier o tematyce fantastyki: Warcrafta 3, Heroes of Might and Magic a także DOTA 2. W grze gracz staje przed zadaniem rozbudowy swojego królestwa, a następnie podboju świata. W WBE nie można ograniczać się do zamknięcia się w jednym mieście i obunkrowania go, zasoby nie są nieskończone co zmusza gracza do ciągłej ekspansji. Prowadzić królestwo do podboju możemy przy pomocy ogromu bohaterów którzy występowali w wielu znanych tytułach, wraz z kolejnymi walkami bohaterowie nabierają umiejętności i mają coraz większy wpływ na rezultat starcia.
+<div class="cytat">
+    <div class="zdjecie">
+        <img src="Grafika/Saint_Exupery.jpeg">
+    </div>
+    <div class="tresc">
+        <p> "Jeżeli nie chcesz mieć swego udziału w klęskach, nie będziesz go miał również w zwycięstwach."</p><br>
+        Antoine de Saint - Exupery
+    </div>
+ 
+</div>
+<div class="niezwlekaj">
+Jeśli nie masz już wątpliwości, to nie zwlekaj i
+</div>
+<a href="rejestracja.php">
+    <div class="zarejestruj">
+        Zarejestruj się teraz!
+    </div>
+    </a>
 <?php include('stopka.php'); ?>
