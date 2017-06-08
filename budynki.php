6a59dfd88e06602a99214e0c3edfcd9994450c59
Veloooo <nazguleczek@gmail.com>
2017-06-08 23:57:38 +0200

    Update budynki.php

--- budynki.php
+++ budynki.php
@@ -1,7 +1,66 @@
 <?php
 	include ('menu2.php');
 ?>
+<div class="zakladka">
+    <div class="zakladka_img">
+        <img src="Grafika/ludzie/budowle/barracks.png">
+    </div>
 
+    <div class="opis">
+        Opis
+    </div>
+    <div class="level">
+        Lvl
+    </div>
+    <div class="ulepsz">
+        UPBUtton
+    </div>
+</div>
+<div class="zakladka">
+    <div class="zakladka_img">
+        <img src="Grafika/ludzie/budowle/altar.png">
+    </div>
+
+    <div class="opis">
+        Opis
+    </div>
+    <div class="level">
+        Lvl
+    </div>
+    <div class="ulepsz">
+        UPBUtton
+    </div>
+</div>
+<div class="zakladka">
+    <div class="zakladka_img">
+        <img src="Grafika/ludzie/budowle/castle.png">
+    </div>
+
+    <div class="opis">
+        Opis
+    </div>
+    <div class="level">
+        Lvl
+    </div>
+    <div class="ulepsz">
+        UPBUtton
+    </div>
+</div>
+<div class="zakladka">
+    <div class="zakladka_img">
+        <img src="Grafika/ludzie/budowle/forge.png">
+    </div>
+
+    <div class="opis">
+        Opis
+    </div>
+    <div class="level">
+        Lvl
+    </div>
+    <div class="ulepsz">
+        UPBUtton
+    </div>
+</div>
 <?php
 	include ('stopka.php');
 ?>
