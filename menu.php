068ac9e66dff694e03fc9196efb164209e550b98
Veloooo <nazguleczek@gmail.com>
2017-06-08 13:19:21 +0200

    Add files via upload

--- menu.php
+++ menu.php
@@ -16,49 +16,39 @@
 <head>
     <meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="Stylesheet" type="text/css" href="style.css" />    
     <script src="script.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/fontello.css">
-</head>
-<body>
-<div id="container">
-        <div id="top">
-            <div class="tpanel"><div id=clock></div></div>
-            <div class="tpanel">b</div>
-            <div class="tpanel" > 
-                <button onclick="document.getElementById('id01').style.display='block'" >Login</button>
-                    <div id="id01" class="modal">
-                        <form class="modal-content animate" action="/action_page.php">
-                            <div class="container">
-                              <label><b>Username</b></label>
-                              <input type="text" placeholder="Login" name="uname" required>
+    <link rel="stylesheet" type="text/css" href="css/fontello.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-                              <label><b>Password</b></label>
-                              <input type="password" placeholder="Hasło" name="psw" required>
-                              <input type="checkbox" checked="checked"> Remember me
-                            <div style="clear:both;"></div>
-                              <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-                              <button type="submit" class="login2">Login</button>
-                                <div style="clear:both;"></div>
-                              <span class="psw"><a href="#">Forgot password?</a></span>
-                            </div>
-                        </form>
-                    </div>
+</head>
+<body background = "Grafika/tlo.jpg">
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2">Data</div>
+                <div class="col-md-2"><div id=clock></div></div>
+                <div class="col-md-2">Imieniny logo</div>
+                <div class="col-md-2">Zarejestrowani</div>
+                <div class="col-md-2">Zaloguj</div>
+                <div class="col-md-2">Aktywni</div>
             </div>
-            <div class="tpanel">d</div>
-            <div class="tpanel">e</div>
         </div>
-        <div style="clear: both"></div>
+
         <div id="nav">
-            <ul>
-                <a href="index.php"><li>Strona główna</li></a>
-                <a href="ogrze.php"><li>O grze</li></a>
-                <a href="otworcach.php"><li>O twórcach</li></a>
-                <a href="regulamin.php"><li>Regulamin</li></a>
-                <a href="opinie.php"><li>Opinie</li></a>
-                <a href="zrzuty.php"><li>Tapety</li></a>
-                <a href="rejestracja.php"><li>Zarejestruj się</li></a>
-            </ul>
+                <a href="index.php"><div id="button">Strona główna</div></a>
+                <a href="ogrze.php"><div id="button">O grze</div></a>
+                <a href="otworcach.php"><div id="button">O twórcach</div></a>
+                <a href="regulamin.php"><div id="button">Regulamin</div></a>
+                <a href="opinie.php"><div id="button">Opinie</div></a>
+                <a href="zrzuty.php"><div id="button">Zrzuty ekranu</div></a>
+                <a href="rejestracja.php"><div id="button">Zarejestruj się</div></a>
         </div>
         
      <div id="center">
