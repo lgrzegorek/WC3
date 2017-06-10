b916cfd7fe34dde14182d4e882d789bb0610fa5b
bartequ <bartlomiej.szabat@gmail.com>
2017-06-10 02:11:44 +0200

    Add files via upload

---
+++ czas.php
@@ -0,0 +1,34 @@
+<?php
+	require_once "connect.php";
+	mysqli_report(MYSQLI_REPORT_STRICT);
+	$user = $_SESSION['user'];
+
+	if (mysql_connect($host, $db_user, $db_password) && mysql_select_db($db_name)) {
+		$wynik = mysql_query("select * from uzytkownicy where user ='$user'")
+		or die ("Błąd w zapytaniu");
+		//mysql_close();
+	}
+	else echo "Nie mogę połączyć się z bazą.";
+		
+	
+	while($ostLog = mysql_fetch_array($wynik)) { 
+        echo "Ostatnie logowanie: ".$ostLog['ostatnieLogowanie']."<br />"; 
+		$_SESSION['ostatnieLogowanie'] = $ostLog['ostatnieLogowanie'];
+	}
+
+	
+	$dataczas = new DateTime();
+	echo "Data i czas serwera: ".$dataczas->format('Y-m-d H:i:s');
+	/*$ostatnie = DateTime::createFromFormat('Y-m-d H:i:s', $_SESSION['ostatnieLogowanie']);
+	$roznica = $dataczas->diff($ostatnie);
+	echo "<br/>Roznica: ".$roznica->format('%d dni, %h godz, %i min %s sek');*/
+	
+	$timeOstatni = strtotime($_SESSION['ostatnieLogowanie']);
+	$timeTeraz = strtotime(date('Y-m-d H:i:s'));
+	$roznicaSekundy = $timeTeraz - $timeOstatni;
+	echo "<br>Roznica w sekundach: ".$roznicaSekundy;
+	
+	$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+	$sql = $polaczenie->query("update uzytkownicy set ostatnieLogowanie=now() where user = '$user'");
+	//Teraz trzeba dodac jakies mnozniki, czas znajduje sie w $roznicaSekundy
+?>
