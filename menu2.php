950845b451725d49bd38c0a45478890327e7fcf6
Veloooo <nazguleczek@gmail.com>
2017-06-10 18:32:38 +0200

    Add files via upload

--- menu2.php
+++ menu2.php
@@ -1,14 +1,3 @@
-<?php
-
-	session_start();
-	
-	if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true))
-	{
-		header('Location: gra.php');
-		exit();
-	}
-
-?>
 
 
 <!DOCTYPE html>
--- surowce.php
+++ surowce.php
@@ -1,6 +1,34 @@
 <?php
 	include ('menu2.php');
 ?>
+<h1> Zasoby </h1>
+<div class="chopki">
+    <img src="Grafika/ludzie/jednostki/worker.png"> Dostępni pracownicy: 10
+</div>
+
+<div class="gold_mine">
+    <h1>Kopalnia złota</h1>
+    <img src="Grafika/zasoby/gold_mine.jpg">
+    <div class="gold_mine_2">
+      <img src="Grafika/ludzie/jednostki/worker.png">  Pracownicy w kopalni złota: <br>
+      <img src="Grafika/icon/mining_level.png" style="width:100px; height:100px;"> Technologia wydobycia złota: <br>
+        Aktualne wydobycie [złoto/min]:
+    </div> 
+    
+</div>
+
+
+<div class="lumber_mill">
+    <h1>Tartak</h1>
+    <img src="Grafika/zasoby/lumber_mill.jpg">
+    <div class="gold_mine_2">
+        <img src="Grafika/ludzie/jednostki/worker.png"> Pracownicy w tartaku:<br>
+        <img src="Grafika/icon/lumber_level.png" style="width:100px; height:100px;"> Technologia drewna: <br>
+        Aktualne pozyskiwanie [drewno/min]:
+    </div>
+    
+</div>
+
 
 <?php
 	include ('stopka.php');
