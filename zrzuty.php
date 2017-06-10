5ccbf3dc030d4af8154a71ac18052eaa910b2d3e
lgrzegorek <grzegorek.lukasz@gmail.com>
2017-06-10 10:23:49 +0200

    Create zrzuty.php

--- zrzuty.php
+++ zrzuty.php
@@ -1,3 +1,13 @@
 <?php include('menu.php'); ?>
 <h1> Zrzuty</h1>
+<div id="screens">
+<img src="Grafika/skriny/warcraft_skrin1.jpg">
+<img src="Grafika/skriny/warcraft_skrin2.jpg">
+<img  src="Grafika/skriny/warcraft_skrin3.jpg">
+<img  src="Grafika/skriny/warcraft_skrin4.jpg">
+<img  src="Grafika/skriny/warcraft_skrin5.jpg">
+<img  src="Grafika/skriny/warcraft_skrin6.jpg">
+
+<div style="clear:both">
+</div>
 <?php include('stopka.php'); ?>
