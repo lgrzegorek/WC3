3cddb49e76b8fdd72fe72f9021b582b33ce3777b
lgrzegorek <grzegorek.lukasz@gmail.com>
2017-06-12 13:32:52 +0200

    Create index.php

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php include('menu.php'); ?>
 <h1>Witaj w naszej grze!</h1>
             Przed Tobą niepowtarzalna szansa zarejestrowania się w tej mega hiper ultra super wciągającej grze, która zaoferuje Ci nieprzespane wieczory, niskie oceny, a także wyrzuci Cię na margines społeczny ale nie będzie Cię to obchodzić bo będziesz graczem najlepszej gry przeglądarkowej w tej części wszechświata. Nie ma na co czekać! Zarejestruj się teraz!
+
+<div class="css-slideshow">
+	<figure>
+		<img src="Grafika/skriny/warcraft_skrin1.jpg"  width="495" height="370"  /><figcaption><strong>IMG1</strong> Walka, krew i łzy! </figcaption> 
+	</figure>
+	<figure>
+		<img src="Grafika/skriny/warcraft_skrin2.jpg"  width="495" height="370"  /><figcaption><strong>IMG2</strong> OPIS ZDJECIA</figcaption> 
+	</figure>
+	<figure>
+		<img src="Grafika/skriny/warcraft_skrin3.jpg"  width="495" height="370"  /><figcaption><strong>IMG3</strong> OPIS ZDJECIA</figcaption> 
+	</figure>
+	<figure>
+		<img src="Grafika/skriny/warcraft_skrin4.jpg"  width="495" height="370"  /><figcaption><strong>IMG4</strong> OPIS ZDJECIA</figcaption> 
+	</figure>
+<figure>
+		<img src="Grafika/skriny/warcraft_skrin5.jpg" width="495" height="370"  /><figcaption><strong>IMG5</strong> OPIS ZDJECIA</figcaption> 
+	</figure>
+	<figure>
+		<img src="Grafika/skriny/warcraft_skrin6.jpg"  width="495" height="370"  /><figcaption><strong>IMG6</strong> OPIS ZDJECIA</figcaption> 
+	</figure>
+	
+  </div>  
+
 <?php include('stopka.php'); ?>
