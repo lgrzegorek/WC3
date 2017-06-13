da4977076870a528628c548bbf2fb0e93313b4d2
lgrzegorek <grzegorek.lukasz@gmail.com>
2017-06-13 09:13:41 +0200

    Create zrzuty.php

--- zrzuty.php
+++ zrzuty.php
@@ -1,13 +1,87 @@
 <?php include('menu.php'); ?>
 <h1> Tapety</h1>
-<div id="screens">
-<img src="Grafika/skriny/warcraft_skrin1.jpg">
-<img src="Grafika/skriny/warcraft_skrin2.jpg">
-<img  src="Grafika/skriny/warcraft_skrin3.jpg">
-<img  src="Grafika/skriny/warcraft_skrin4.jpg">
-<img  src="Grafika/skriny/warcraft_skrin5.jpg">
-<img  src="Grafika/skriny/warcraft_skrin6.jpg">
-
-<div style="clear:both">
+<div class="row">
+<div class="column">
+<img src="Grafika/skriny/warcraft_skrin1.jpg" onclick="openModal();currentSlide(1)" class="hover-shadow cursor" style="width:200px; height:150px;">
 </div>
+<div class="column">
+<img src="Grafika/skriny/warcraft_skrin2.jpg" onclick="openModal();currentSlide(2)" class="hover-shadow cursor" style="width:200px; height:150px;">
+</div>
+<div class="column">
+<img  src="Grafika/skriny/warcraft_skrin3.jpg" onclick="openModal();currentSlide(3)" class="hover-shadow cursor" style="width:200px; height:150px;">
+</div>
+<div class="column">
+<img  src="Grafika/skriny/warcraft_skrin4.jpg" onclick="openModal();currentSlide(4)" class="hover-shadow cursor" style="width:200px; height:150px;">
+</div>
+<div class="column">
+<img  src="Grafika/skriny/warcraft_skrin5.jpg" onclick="openModal();currentSlide(5)" class="hover-shadow cursor" style="width:200px; height:150px;">
+</div>
+<div class="column">
+<img  src="Grafika/skriny/warcraft_skrin6.jpg" onclick="openModal();currentSlide(6)" class="hover-shadow cursor" style="width:200px; height:150px;">
+</div>
+
+    
+<div id="myModal" class="modal">
+  <span class="close" onclick="closeModal()">&#10060;</span>
+  <div class="modal-content">
+
+    <div class="mySlides">
+      <div class="numbertext">1 / 6</div>
+      <img src="Grafika/skriny/warcraft_skrin1.jpg" style="width:500px;  height:450px;">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">2 / 6</div>
+      <img src="Grafika/skriny/warcraft_skrin2.jpg" style="width:500px; height:450px;">
+    </div>
+
+    <div class="mySlides">
+      <div class="numbertext">3 / 6</div>
+      <img src="Grafika/skriny/warcraft_skrin3.jpg" style="width:500px;  height:450px;">
+    </div>
+    
+    <div class="mySlides">
+      <div class="numbertext">4 / 6</div>
+      <img src="Grafika/skriny/warcraft_skrin4.jpg" style="width:500px;  height:450px;">
+    </div>
+      
+    <div class="mySlides">
+      <div class="numbertext">5 / 6</div>
+      <img src="Grafika/skriny/warcraft_skrin5.jpg" style="width:500px; height:450px;">
+    </div>
+      
+    <div class="mySlides">
+      <div class="numbertext">6 / 6</div>
+      <img src="Grafika/skriny/warcraft_skrin6.jpg" style="width:500px; height:450px;">
+    </div>
+    
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <div class="caption-container">
+      <p id="caption"></p>
+    </div>
+
+
+    <div class="column">
+      <img class="demo cursor" src="Grafika/skriny/warcraft_skrin1.jpg" style="width:200px;  height:150px; " onclick="currentSlide(1)">
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="Grafika/skriny/warcraft_skrin2.jpg" style="width:200px;  height:150px;" onclick="currentSlide(2)" >
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="Grafika/skriny/warcraft_skrin3.jpg" style="width:200px;  height:150px;" onclick="currentSlide(3)" >
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="Grafika/skriny/warcraft_skrin4.jpg" style="width:200px;  height:150px;" onclick="currentSlide(4)" >
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="Grafika/skriny/warcraft_skrin5.jpg" style="width:200px;  height:150px;" onclick="currentSlide(3)" >
+    </div>
+    <div class="column">
+      <img class="demo cursor" src="Grafika/skriny/warcraft_skrin6.jpg" style="width:200px;  height:150px;" onclick="currentSlide(4)" >
+    </div>
+  </div>
+</div>
+  
 <?php include('stopka.php'); ?>
