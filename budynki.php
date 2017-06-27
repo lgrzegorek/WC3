b53bae25468c9cd6e8b3aa9a00c36c55c56fe21f
Veloooo <nazguleczek@gmail.com>
2017-06-27 19:08:28 +0200

    Add files via upload

--- budynki.php
+++ budynki.php
@@ -1,6 +1,6 @@
 <?php
 	include ('menu2.php');
-    include ('skrypt.php');
+ 
 ?>
 
 <div class="zakladka_opis">
@@ -17,7 +17,7 @@
         Obraz:
     </div>
     <div class="opis">
-        Opis
+        Opis 
     </div>
   <div class="wymagania">Wymagania:</div>
     <div class="chopki"><img src="Grafika/ludzie/jednostki/worker.png"></div>
@@ -42,12 +42,12 @@
     <div class="opis">
         Ratusz pozwala na rekrutację robotników.
     </div>
-  <div class="wymagania"><?php oblicz($_SESSION['castle_lvl'],500,500)?></div>
+  <div class="wymagania"><?php oblicz($_SESSION['castle_lvl'],500,500,true)?></div>
     <input type="text" style="width: 30px;">
     <div class="ulepsz" id="upRatusz">
-      <?php wypisz()?>
+      <?php wypisz(($_SESSION['castle_lvl']+1),500,500,false)?>
     </div>
-    <div class="czas">3h 22m</div>
+    <div class="czas"><?php czas(($_SESSION['castle_lvl']+1),60) ?></div>
 </div>
 <div class="zakladka">
      
@@ -65,12 +65,12 @@
     <div class="opis">
         Koszary umożliwiają rekrutację jednostek, wraz ze wzrostem poziomu rekrutacja przebiega szybciej, a także daje możliwość rekrutacji kolejnych jednostek.
     </div>
-    <div class="wymagania"><?php oblicz($_SESSION['barrack_lvl'],300,350)?></div>
+    <div class="wymagania"><?php oblicz($_SESSION['barrack_lvl'],300,350,true)?></div>
     <input type="text" style="width: 30px;">
     <div class="ulepsz">
-         <button type="button" class="btn btn-success">Ulepsz</button>
+          <?php wypisz(($_SESSION['barrack_lvl']+1),500,500,false)?>
     </div>
-    <div class="czas">3h 22m</div>
+      <div class="czas"><?php czas(($_SESSION['barrack_lvl']+1),50) ?></div>
 </div>
 <div class="zakladka">
    
@@ -88,12 +88,12 @@
     <div class="opis">
         Ratusz pozwala na przywołanie, a także wskrzeszenie bohaterów. Co 10 poziomów możliwe jest przywołanie kolejnego bohatera.
     </div>
-    <div class="wymagania"><?php oblicz($_SESSION['altar_lvl'],300,150)?></div>
+    <div class="wymagania"><?php oblicz($_SESSION['altar_lvl'],300,150,true)?></div>
      <input type="text" style="width: 30px;">
     <div class="ulepsz">
-        <button type="button" class="btn btn-success">Ulepsz</button>
+        <?php wypisz(($_SESSION['altar_lvl']+1),500,500,false)?>
     </div>
-    <div class="czas">3h 22m</div>
+     <div class="czas"><?php czas(($_SESSION['altar_lvl']+1),40) ?></div>
 </div>
 <div class="zakladka">
     
@@ -111,12 +111,12 @@
     <div class="opis">
         Kuźnia pozwala na dokonywanie badań w zakresie ulepszeń jednostek, wyższe poziomy kuźni odblokowują wyższe poziomy ulepszeń.
     </div>
-     <div class="wymagania"><?php oblicz($_SESSION['forge_lvl'],300,300)?></div>
+     <div class="wymagania"><?php oblicz($_SESSION['forge_lvl'],300,300,true)?></div>
      <input type="text" style="width: 30px;">
     <div class="ulepsz">
-        <button type="button" class="btn btn-success">Ulepsz</button>
+         <?php wypisz(($_SESSION['forge_lvl']+1),500,500,false)?>
     </div>
-   <div class="czas">3h 22m</div>
+    <div class="czas"><?php czas(($_SESSION['forge_lvl']+1),40) ?></div>
 </div>
 <div class="zakladka">
     
@@ -134,12 +134,12 @@
     <div class="opis">
         Farmy wytwarzają żywność, która jest potrzebna do utrzymania jednostek. Każda farma dostarcza 10 jednostek pożywienia.
     </div>
-     <div class="wymagania"><?php oblicz($_SESSION['house_lvl'],250,300)?></div>
+     <div class="wymagania"><?php oblicz($_SESSION['house_lvl'],250,300,true)?></div>
      <input type="text" style="width: 30px;">
     <div class="ulepsz">
-        <button type="button" class="btn btn-success">Buduj</button>
+         <?php wypisz(($_SESSION['house_lvl']+1),500,500,false)?>
     </div>
-    <div class="czas">3h 22m</div>
+     <div class="czas"><?php czas(($_SESSION['house_lvl']+1),50) ?></div>
 </div>
 <?php
 	include ('stopka.php');
