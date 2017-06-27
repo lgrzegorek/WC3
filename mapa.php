ad4f8b55713fa72002668efc8a56196bc87d70ae
amazinghat <olek.gawin@gmail.com>
2017-06-27 18:21:06 +0200

    klikajaca mapa

--- mapa.php
+++ mapa.php
@@ -2,7 +2,51 @@
 	include ('menu2.php');
 ?>
 
-<img src="Grafika/mapfull.png">
+<img src="Grafika/mapfull.png" usemap="#citymap">
+
+<map name="citymap">
+	<area shape="rect" coords="22,22,249,193" href="podglad.php">
+	<area shape="rect" coords="250,22,478,193" href="">
+	<area shape="rect" coords="480,22,708,193" href="">
+	<area shape="rect" coords="710,22,938,193" href="">
+	<area shape="rect" coords="940,22,1168,193" href="">
+	<area shape="rect" coords="1170,22,1400,193" href="">
+
+	<area shape="rect" coords="22,198,249,367" href="">
+	<area shape="rect" coords="250,198,478,367" href="">
+	<area shape="rect" coords="480,198,708,367" href="">
+	<area shape="rect" coords="710,198,938,367" href="">
+	<area shape="rect" coords="940,198,1168,367" href="">
+	<area shape="rect" coords="1170,198,1400,367" href="">
+
+	<area shape="rect" coords="22,370,249,540" href="">
+	<area shape="rect" coords="250,370,478,540" href="">
+	<area shape="rect" coords="480,370,708,540" href="">
+	<area shape="rect" coords="710,370,938,540" href="">
+	<area shape="rect" coords="940,370,1168,540" href="">
+	<area shape="rect" coords="1170,370,1400,540" href="">
+
+	<area shape="rect" coords="22,543,249,713" href="">
+	<area shape="rect" coords="250,543,478,713" href="">
+	<area shape="rect" coords="480,543,708,713" href="">
+	<area shape="rect" coords="710,543,938,713" href="">
+	<area shape="rect" coords="940,543,1168,713" href="">
+	<area shape="rect" coords="1170,543,1400,713" href="">
+
+	<area shape="rect" coords="22,718,249,888" href="">
+	<area shape="rect" coords="250,718,478,888" href="">
+	<area shape="rect" coords="480,718,708,888" href="">
+	<area shape="rect" coords="710,718,938,888" href="">
+	<area shape="rect" coords="940,718,1168,888" href="">
+	<area shape="rect" coords="1170,718,1400,888" href="">
+
+	<area shape="rect" coords="22,893,249,1066" href="">
+	<area shape="rect" coords="250,893,478,1066" href="">
+	<area shape="rect" coords="480,893,708,1066" href="">
+	<area shape="rect" coords="710,893,938,1066" href="">
+	<area shape="rect" coords="940,893,1168,1066" href="">
+	<area shape="rect" coords="1170,893,1400,1066" href="">
+</map>
 
 <?php
 	include ('stopka.php');
