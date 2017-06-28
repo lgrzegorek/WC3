904670a310b14e67d87067e85ad479516a1ac9b4
Veloooo <nazguleczek@gmail.com>
2017-06-28 16:09:13 +0200

    Update skrypt.php

--- skrypt.php
+++ skrypt.php
@@ -1,23 +1,60 @@
 <?php 
-function oblicz($lvl, $zloto_lvl1, $drewno_lvl1){
-    if($lvl==0){
-        echo "Złoto: $zloto_lvl1 <br> Drewno: $drewno_lvl1";
+function oblicz($lvl,$budynek,$a){
+    $pow=pow(1.5,$lvl);
+    switch ($budynek){
+        case 'castle': $zloto=500; $drewno=500; break;
+        case 'barrack': $zloto=300; $drewno=350; break;
+        case 'altar': $zloto=300; $drewno=150; break;
+        case 'forge': $zloto=300; $drewno=300; break;
+        case 'house': $zloto=250; $drewno=300; break;
     }
-    else
-    {   
-        $gold=(int)pow(1.5,$lvl)*$zloto_lvl1;
-        $wood=(int)pow(1.5,$lvl)*$drewno_lvl1;
-        echo "Złoto: $gold <br> Drewno: $wood";
+    $zloto=$zloto*$pow;
+    $drewno=$drewno*$pow;
+    if ($a){
+        echo "Złoto: ".$zloto."<br> Drewno: ".$drewno;
     }
+    $zasob['a']=$zloto;
+    $zasob['b']=$drewno;
+    return $zasob;    
 }
 
-function wypisz(){
-        
-    if($_SESSION['drewno']>=100 && $_SESSION['zloto']>=100){
+function czas($lvl, $budynek){
+    $pow=pow(1.5,$lvl);
+    
+    switch($budynek){
+        case 'castle': $minuty=(int)($pow*60); break;
+        case 'barrack': $minuty=(int)($pow*50);break;
+        case 'altar': $minuty=(int)($pow*40);break;
+        case 'forge': $minuty=(int)($pow*50);break;
+        case 'house': $minuty=(int)($pow*40);break;
+    }
+    
+    $godziny=$minuty/60;
+    $godziny=floor($godziny);
+    $minuty=$minuty%60;
+
+    if ($godziny==0){ 
+        echo $minuty."min.";
+    }
+    if ($minuty==0){ 
+        echo $godziny."godź.";
+    }
+    else{ 
+        echo $godziny."godź. <br>".$minuty."min.";
+    }  
+    
+}
+
+function wypisz($lvl,$budynek){
+    $zasoby=oblicz($lvl,$budynek,false);  
+    
+    $zloto=$zasoby['a'];
+    $drewno=$zasoby['b'];
+    if($_SESSION['drewno']>=$drewno && $_SESSION['zloto']>=$zloto){
         echo  '<button type="button" class="btn btn-success">Ulepsz</button>';
     }
     else{
-        echo  '<button type="button" class="btn btn-danger">Ulepsz</button>';
+        echo  '<button type="button" class="btn btn-danger">Brak zasobów</button>';
     }
 }
 
