602805e17401cb8b9adae1a402d4646fb1218179
bartequ <bartlomiej.szabat@gmail.com>
2017-06-28 16:08:37 +0200

    Update rejestracja.php

--- rejestracja.php
+++ rejestracja.php
@@ -52,7 +52,7 @@
 		mysqli_report(MYSQLI_REPORT_STRICT);
 		
 		try {
-			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
+=			$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
 			$polaczenie1 = new mysqli($host, $db_user, $db_password, $db_name);
 			$polaczenie2 = new mysqli($host, $db_user, $db_password, $db_name);
 			if ($polaczenie->connect_errno!=0) {
@@ -81,11 +81,11 @@
 				//Jesli walidacja poprawna
 				if ($flaga) {
 					if ($polaczenie->query("insert into uzytkownicy values (NULL, '$nick', '$haslo_hash', '$email',100, 100, 100, now())") && $polaczenie1->query("insert into budynki values ('$nick', '$rasa', 0, 0, 0, 0, 0, 0, 0)")){
-						if ($rasa = "Orkowie" && $polaczenie2->query("insert into orkowie values('$nick',0,0,0,0,0,0,0)")) {
+						if ($rasa == "Orkowie" && $polaczenie2->query("insert into orkowie values('$nick',0,0,0,0,0,0,0)")) {
 							$_SESSION['udanarejestracja'] = true;
 							header("Location: podglad.php");
 						}
-						else if ($rasa = "Ludzie" && $polaczenie2->query("insert into ludzie values('$nick',0,0,0,0,0,0,0)")) {
+						else if ($rasa == "Ludzie" && $polaczenie2->query("insert into ludzie values('$nick',0,0,0,0,0,0,0)")) {
 							$_SESSION['udanarejestracja'] = true;
 							header("Location: podglad.php");
 						}
