117d7d79f6c4a620dd514bac7c253f1b3d50eac6
Veloooo <nazguleczek@gmail.com>
2017-10-03 15:56:38 +0200

    Update podglad.php

--- podglad.php
+++ podglad.php
@@ -27,14 +27,14 @@ $row = $result->fetch_assoc();
 ?>
 
 <div>
-	<div class="kolejki">
-	Kolejka budowania: <?php echo $row['budowy'];?>
+	<div id="budowy" class="kolejki">
+	Kolejka budowania: <br>
 	</div>
-	<div class="kolejki">
-	Kolejka badań: <?php echo $row['badania'];?>
+	<div id="badania" class="kolejki">
+	Kolejka badań: <br>
 	</div>
-	<div class="kolejki">
-	Kolejka jednostek: <?php echo $row['jednostek'];?>
+	<div id="kolejka_jednostki" class="kolejki" >
+    Kolejka jednostek: <br>
 	</div>
 </div>
 <div>
@@ -45,3 +45,37 @@ $row = $result->fetch_assoc();
 <?php
 	include ('stopka.php');
 ?>
+<script>
+    for(var i=0; i<10; i++){
+        $('#kolejka_jednostki').append("<div id='kolejka_jednostki"+i+"' class='kolejka_j'> x</div>");
+    }
+    var rasa=<?php echo json_encode($_SESSION['rasa']); ?>;
+    var rekrutacja=<?php echo json_encode($_SESSION['kolejka_jednostek']); ?>.split("-");
+    var budynek=<?php echo json_encode($_SESSION['budynek']); ?>;
+    var badanie=<?php echo json_encode($_SESSION['badanie']); ?>;
+    if(rasa.equals("orkowie")){
+        switch(budynek){
+             
+        }
+    }
+    for (var i=1; i<11; i++){
+        var obraz="";
+        if(rekrutacja[i-1]){
+                switch(rekrutacja[i-1]){
+                    case "siepacze": obraz="grunt"; break;
+                    case "lowcy_glow": obraz="headhunter"; break;
+                    case "taureni": obraz="tauren"; break;
+                    case "kodo": obraz="kodo"; break;
+                    case "szamani": obraz="shaman"; break;
+                    case "jezdzcy": obraz="bat"; break;
+                    case "miecznicy": obraz="footman"; break;
+                    case "strzelcy": obraz="sniper"; break;
+                    case "rycerze": obraz="knight"; break;
+                    case "kanonierzy": obraz="canon"; break;
+                    case "czarodziejki": obraz="wizard"; break;
+                    case "zyrokoptery": obraz="gyro"; break;
+                }          
+            }
+            document.getElementById("kolejka_jednostki"+(i-1)).style.backgroundImage="url('Grafika/"+rasa+"/jednostki/"+obraz+".png')";
+    }
+</script>
